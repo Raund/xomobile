b2939d63a7677fc0f425167628450881121ba9db
Sergey <vor.ser87@gmail.com>
2017-07-04 23:34:02 +0300

    create main

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -46,14 +46,14 @@
                             <div class="bar__module">
                                 <ul class="menu-horizontal text-left">
                                     @if($categories_data['benefits']->active == 1)
-                                        <li> <a class="r-menu-link" data-scroll-id="about-us" href="/{{ App::getLocale() }}#about-us">{{ trans('base.about_us') }}</a> </li>
+                                        <li> <a class="r-menu-link" data-scroll-id="about-us" href="/{{ App::getLocale() }}#about-us">{{ $categories_data['about']->getTranslate('title') }}</a> </li>
                                     @endif
-                                        <li> <a class="r-menu-link" data-scroll-id="prices" href="/{{ App::getLocale() }}#prices">{{ trans('base.prices') }}</a> </li>
+                                        <li> <a class="r-menu-link" data-scroll-id="prices" href="/{{ App::getLocale() }}#prices">{{ $categories_data['price']->getTranslate('title') }}</a> </li>
                                     @if( count($download) !== 0 AND $categories_data['download']->active == 1)
-                                        <li> <a class="r-menu-link" data-scroll-id="download" href="/{{ App::getLocale() }}#download">{{ trans('base.download') }}</a> </li>
+                                        <li> <a class="r-menu-link" data-scroll-id="download" href="/{{ App::getLocale() }}#download">{{ $categories_data['download']->getTranslate('title') }}</a> </li>
                                     @endif
                                     @if(count($contact) !== 0 AND $categories_data['contact']->active == 1)
-                                        <li> <a class="r-menu-link" data-scroll-id="contacts" href="/{{ App::getLocale() }}#contacts">{{ trans('base.contacts') }}</a> </li>
+                                        <li> <a class="r-menu-link" data-scroll-id="contacts" href="/{{ App::getLocale() }}#contacts">{{ $categories_data['contact']->getTranslate('title') }}</a> </li>
                                     @endif
                                 </ul>
                             </div>
