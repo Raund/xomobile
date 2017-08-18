58180024eab5f7e05108547b889e2bf68aa8f28f
Andrii Melnichuk <andrewlutsk88@gmail.com>
2017-08-18 16:39:22 +0300

    fix lang

--- resources/views/frontend/header_content.blade.php
+++ resources/views/frontend/header_content.blade.php
@@ -6,7 +6,7 @@
                 </div>
                 <div class="col-xs-6">
                     <div class="lang text-right">
-                        <a class="active-lang" href="#"><img src="/{{ asset('/img/frontend/en.png') }}"></a>
+                        <a class="active-lang" href="#"><img src="{{ asset('/img/frontend/en.png') }}"></a>
                         <ul class="langs">
                             @foreach($langs as $lang)
                                 <li> <a href="{{str_replace(url(App::getLocale()), url($lang->lang), Request::url())}}"><img src="/{{ $lang->img }}" alt="{{ $lang->lang }}"></a></li>
